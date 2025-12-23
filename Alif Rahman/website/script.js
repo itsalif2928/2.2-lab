0f5ffd38ff8dfdc9dfaaed9357ec2dc228597bc0
Md Asif Rahman Alif <mdalifr2928@gmail.com>
2025-12-24 00:51:51 +0600

    Delete Alif Rahman/website directory

--- Alif Rahman/website/script.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-    alert("Welcome to Alif Rahman's Website!");
-}
-
-document.getElementById("contactForm")?.addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Message sent successfully!");
-    this.reset();
-});
